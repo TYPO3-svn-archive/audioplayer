0a8bb77ef0933f6e2d8a86e05d238ff3fd9d52e3
peschuster <peschuster@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-06-27 13:33:18 +0000

    initial import auto generated code by kickstarter
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/audioplayer/trunk@10261 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "audioplayer"
+#
+# Auto generated 27-06-2008 15:12
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'MP3 Audio Player',
+	'description' => 'Ported "Audio Player Wordpress plugin" of 1pixelout.net',
+	'category' => 'plugin',
+	'author' => 'Peter Schuster',
+	'author_email' => 'typo3@peschuster.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"c8ba";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"9090";s:14:"ext_tables.php";s:4:"5d69";s:13:"locallang.xml";s:4:"1163";s:16:"locallang_db.xml";s:4:"b139";s:19:"doc/wizard_form.dat";s:4:"49c7";s:20:"doc/wizard_form.html";s:4:"3d58";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:32:"pi1/class.tx_audioplayer_pi1.php";s:4:"7e8a";s:40:"pi1/class.tx_audioplayer_pi1_wizicon.php";s:4:"cbed";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"6e55";s:24:"pi1/static/editorcfg.txt";s:4:"3c79";}',
+);
+
+?>
