d95208dc206f6e669b97130101d61925010167da
peschuster <peschuster@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-06-30 21:04:15 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/audioplayer/trunk@10320 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "audioplayer"
 #
-# Auto generated 27-06-2008 16:39
+# Auto generated 30-06-2008 13:17
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -12,24 +12,26 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'MP3 Audio Player API',
-	'description' => 'Ported "Audio Player Wordpress plugin" of 1pixelout.net as api for other extensions.',
+	'description' => 'This extension provides an api for other extensions to display a mp3 flash audio player. The flash player is a standalone version of the "Audio Player Wordpress plugin" of 1pixelout.net.',
 	'category' => 'misc',
-	'author' => 'Peter Schuster',
-	'author_email' => 'typo3@peschuster.de',
-	'shy' => '',
+	'shy' => 0,
+	'version' => '0.1.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
 	'module' => '',
 	'state' => 'beta',
-	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
+	'clearcacheonload' => 0,
 	'lockType' => '',
+	'author' => 'Peter Schuster',
+	'author_email' => 'typo3@peschuster.de',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,6 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"3619";s:10:"README.txt";s:4:"748d";s:12:"api_doc.html";s:4:"b326";s:24:"class.tx_audioplayer.php";s:4:"e3ca";s:12:"ext_icon.gif";s:4:"7aae";s:15:"ext_php_api.dat";s:4:"16d2";s:14:"doc/manual.sxw";s:4:"7779";s:19:"res/audio-player.js";s:4:"bcdf";s:14:"res/player.swf";s:4:"7e74";s:33:"tests/tx_audioplayer_testcase.php";s:4:"cbc7";}',
 );
 
 ?>
